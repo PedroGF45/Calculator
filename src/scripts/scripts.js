dc59b995cbaa585c487a1564b0201ed3e6a0e039
Pedro Brito <pedrobfh@gmail.com>
2021-08-12 13:39:44 +0100

    different approach on operate and evaluate functions

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,26 +1,29 @@
-//tentar fazer com a,b
-
+//procurar fazer com based on string
 
 
 const defaultNumber = null;
 
 let currentNumber = null;
 let previousNumber = null;
+let currentOperator = "";
+
+//updates de current operator being used
+function updateOperator(operand) {
+    currentOperator = operand;
+    cache.innerText += `${operand}`;
+}
 
 //updates the number in the display bottom portion
 function updateDisplay(num) {
     display.innerText = `${num}`;
-    if (currentNumber != null) {
-        previousNumber = Number(num);
-    } else {
-        currentNumber = Number(num);
-    }
+    (previousNumber == null) ? previousNumber = Number(num) : currentNumber = Number(num);
     updateCache(`${num}`);
 }
 
 //updates the number in the display top portion
 function updateCache(value) {
     cache.innerText += `${value}`;
+    console.log(cache);
 }
 
 
@@ -32,7 +35,7 @@ let cache = document.getElementById("cache");
 const clear = document.getElementById("clear");
 
 //Add listener onclick and sends the respective function
-operator.forEach(operator => operator.addEventListener("click", () => operate(operator.getAttribute("id"), currentNumber, previousNumber)));
+operator.forEach(operator => operator.addEventListener("click", () => operate(operator.getAttribute("id"))));
 number.forEach(number => number.addEventListener("click", () => updateDisplay(number.getAttribute("value"))));
 clear.addEventListener("click", clearDisplay);
 
@@ -47,26 +50,19 @@ function clearDisplay() {
 
 //operator functions
 function add(a, b) {
-    updateCache(" + ");
-    display.innerText = currentNumber;
-    if (a != null && b != null) {
         let result = a + b;
         display.innerText = result;
-        currentNumber = result;
-        previousNumber = null;
-    }  
+        previousNumber = result;
+        currentNumber = null;
+        cache.innerText = `${result}+`;
 }
 
 function subtract(a, b) {
-    updateCache(" - ");
-    display.innerText = currentNumber;
-    if (a != null && b != null) {
-        let result = (a - b);
-        console.log(`${a} - ${b} = ${result}`);
-        display.innerText = result;
-        currentNumber = result;
-        previousNumber = null;
-    } 
+    let result = (a-b);
+    display.innerText = result;
+    previousNumber = result;
+    currentNumber = null;
+    cache.innerText = `${result}-`;
 }
 
 function multiply(a, b) {
@@ -92,16 +88,32 @@ function divide(a, b){
 }
 
 //choses the operation based on id of the button clicked
-function operate(operand, currentNumber, previousNumber) {
-    if (operand === "plus") {
-        add(currentNumber, previousNumber);
-    } else if (operand === "minus") {
-        subtract(currentNumber, previousNumber);
-    } else if (operand === "multiply") {
-        multiply(currentNumber, previousNumber);
-    } else if (operand === "divide") {
-        divide(currentNumber, previousNumber);
+function operate(operand) {
+    switch (operand) {
+        case "plus":
+            updateOperator("+");
+            evaluate();
+            break;
+        case "minus":
+            updateOperator("-");
+            evaluate();
+            break;
+        case "multiply":
+            updateOperator("x");
+            evaluate();
+            break;
+        case "divide":
+            updateOperator("/");
+            evaluate();
+            break;
     }
 }
 
 
+function evaluate() {
+    if (currentOperator == "+" && cache.innerText.length > 2) {
+        add(previousNumber, currentNumber);
+    } else if (currentOperator == "-" && cache.innerText.length > 2) {
+        subtract(previousNumber, currentNumber);
+    }
+}
